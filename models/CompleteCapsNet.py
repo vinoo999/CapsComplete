c77cc0434529d4dbf33a7ba6daa333cff1b10bd7
Vinay Ramesh <vinoo999@aim.com>
2019-04-28 20:35:47 -0400

    CapsComplete now based in vector caps for time reasons

--- models/CompleteCapsNet.py
+++ models/CompleteCapsNet.py
@@ -1,3 +1,22 @@
+# Copyright 2018 The CapsLayer Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==========================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 import numpy as np
 import capslayer as cl
 import tensorflow as tf
@@ -22,7 +41,7 @@ class CompleteCapsNet(object):
             inputs: Tensor or array with shape [batch_size, height, width, channels] or [batch_size, height * width * channels].
             labels: Tensor or array with shape [batch_size].
         Returns:
-            poses: Tensor with shape [batch_size, num_label, 16, 1].
+            poses: [batch_size, num_label, 16, 1].
             probs: Tensor with shape [batch_size, num_label], the probability of entity presence.
         """
         m_plus = kwargs.get('m_plus', 0.9)
@@ -34,59 +53,38 @@ class CompleteCapsNet(object):
 
         self.raw_imgs = inputs
         self.labels = labels
-        probs = []
-        inputs = tf.reshape(inputs, shape=[-1, self.height, self.width, self.channels])
-        conv1 = tf.layers.conv2d(inputs,
-                                 filters=32,
-                                 kernel_size=5,
-                                 strides=2,
-                                 padding='VALID',
-                                 activation=tf.nn.relu,
-                                 name="Conv1_layer")
+        with tf.variable_scope('Conv1_layer'):
+            # Conv1, return with shape [batch_size, 20, 20, 256]
+            inputs = tf.reshape(self.raw_imgs, shape=[-1, self.height, self.width, self.channels])
+            conv1 = tf.layers.conv2d(inputs,
+                                     filters=256,
+                                     kernel_size=9,
+                                     strides=1,
+                                     padding='VALID',
+                                     activation=tf.nn.relu)
 
-        convCaps, activation = cl.layers.primaryCaps(conv1,
-                                                     filters=32,
-                                                     kernel_size=1,
-                                                     strides=1,
-                                                     out_caps_dims=[4, 4],
-                                                     method="logistic",
-                                                     name="PrimaryCaps_layer")
+        with tf.variable_scope('PrimaryCaps_layer'):
+            primaryCaps, activation = cl.layers.primaryCaps(conv1,
+                                                            filters=32,
+                                                            kernel_size=9,
+                                                            strides=2,
+                                                            out_caps_dims=[8, 1],
+                                                            method="norm")
 
-        probs.append(tf.reduce_mean(activation))
-        routing_method = 'EMRouting'
-        convCaps, activation = cl.layers.conv2d(convCaps,
-                                                activation,
-                                                filters=32,
-                                                out_caps_dims=[4, 4],
-                                                kernel_size=(3, 3),
-                                                strides=(2, 2),
-                                                routing_method=routing_method,
-                                                name="ConvCaps1_layer")
-
-        probs.append(tf.reduce_mean(activation))
-        convCaps, activation = cl.layers.conv2d(convCaps,
-                                                activation,
-                                                filters=32,
-                                                out_caps_dims=[4, 4],
-                                                kernel_size=(3, 3),
-                                                strides=(1, 1),
-                                                routing_method=routing_method,
-                                                name="ConvCaps2_layer")
-
-        probs.append(tf.reduce_mean(activation))
-        self.poses, self.probs = cl.layers.dense(convCaps,
-                                                 activation,
-                                                 num_outputs=self.num_label,
-                                                 out_caps_dims=[4, 4],
-                                                 routing_method=routing_method,
-                                                 coordinate_addition=True,
-                                                 name="ClassCaps_layer")
-        probs.append(tf.reduce_mean(self.probs))
-        tf.summary.scalar("probs", tf.reduce_mean(probs))
+        with tf.variable_scope('DigitCaps_layer'):
+            routing_method = "EMRouting"
+            num_inputs = np.prod(cl.shape(primaryCaps)[1:4])
+            primaryCaps = tf.reshape(primaryCaps, shape=[-1, num_inputs, 8, 1])
+            activation = tf.reshape(activation, shape=[-1, num_inputs])
+            self.poses, self.probs = cl.layers.dense(primaryCaps,
+                                                     activation,
+                                                     num_outputs=self.num_label,
+                                                     out_caps_dims=[16, 1],
+                                                     routing_method=routing_method)
+            cl.summary.histogram('activation', self.probs, verbose=summary_verbose)
 
         # Decoder structure
         # Reconstructe the inputs with 3 FC layers
-        # [batch_size, 1, 16, 1] => [batch_size, 16] => [batch_size, 512]
         with tf.variable_scope('Decoder'):
             labels = tf.one_hot(self.labels, depth=self.num_label, axis=-1, dtype=tf.float32)
             self.labels_one_hoted = tf.reshape(labels, (-1, self.num_label, 1, 1))
@@ -103,7 +101,6 @@ class CompleteCapsNet(object):
             cl.summary.image('reconstruction_img', recon_imgs, verbose=summary_verbose)
 
         with tf.variable_scope('accuracy'):
-            cl.summary.histogram('activation', tf.nn.softmax(self.probs, 1), verbose=summary_verbose)
             logits_idx = tf.to_int32(tf.argmax(cl.softmax(self.probs, axis=1), axis=1))
             correct_prediction = tf.equal(tf.to_int32(self.labels), logits_idx)
             correct = tf.reduce_sum(tf.cast(correct_prediction, tf.float32))
@@ -121,28 +118,26 @@ class CompleteCapsNet(object):
         regularization_scale = kwargs.get('regularization_scale', 0.392)
 
         with tf.variable_scope("loss"):
-            # The reconstruction loss
+            # 1. Margin loss
+            self.classification_loss = cl.losses.margin_loss(logits=self.probs,
+                                                labels=tf.squeeze(self.labels_one_hoted, axis=(2, 3)))
+            cl.summary.scalar('margin_loss', margin_loss, verbose=summary_verbose)
+
+            # 2. The reconstruction loss
             orgin = tf.reshape(self.raw_imgs, shape=(-1, self.height * self.width * self.channels))
             squared = tf.square(self.recon_imgs - orgin)
-            reconstruction_err = tf.reduce_mean(squared)
+            self.reconstruction_err = tf.reduce_mean(squared)
             cl.summary.scalar('reconstruction_loss', reconstruction_err, verbose=summary_verbose)
 
-            # Spread loss
-            initial_margin = 0.2
-            max_margin = 0.9
-            interstep = 8000
-            margin = (self.global_step / interstep) * 0.1 + initial_margin
-            margin = tf.cast(tf.minimum(margin, max_margin), tf.float32)
-            cl.summary.scalar('margin', tf.reduce_mean(margin), verbose=summary_verbose)
-            spread_loss = cl.losses.spread_loss(logits=self.probs,
-                                                labels=tf.squeeze(self.labels_one_hoted, axis=(2, 3)),
-                                                margin=margin)
-            cl.summary.scalar('spread_loss', spread_loss, verbose=summary_verbose)
+            # 3. Total loss
+            # The paper uses sum of squared error as reconstruction error, but we
+            # have used reduce_mean in `# 2 The reconstruction loss` to calculate
+            # mean squared error. In order to keep in line with the paper,the
+            # regularization scale should be 0.0005*784=0.392
+            self.total_loss = self.classification_loss + regularization_scale * self.reconstruction_err
 
-            # Total loss
-            total_loss = spread_loss + regularization_scale * reconstruction_err
             cl.summary.scalar('total_loss', total_loss, verbose=summary_verbose)
-            return total_loss
+            return self.total_loss
 
     def train(self, optimizer, num_gpus=1):
         self.global_step = tf.Variable(1, name='global_step', trainable=False)
